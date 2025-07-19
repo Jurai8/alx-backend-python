4c182b1e6fd91745e94a3089c6dc39277b18dc83
Charles <131194505+Jurai8@users.noreply.github.com>
2025-07-19 18:04:35 +0200

    getjson

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import utils
+from utils import memoize
 from unittest.mock import patch, Mock, MagicMock
 from parameterized import parameterized, parameterized_class
 import unittest
@@ -15,7 +16,6 @@ class TestAccessNestedMap(unittest.TestCase):
     ]) 
     def test_access_nested_map(self, nested_map, path, result):
         self.assertEqual(utils.access_nested_map(nested_map, path), result)
-
 
     @parameterized.expand([
         ({}, ("a"), (KeyError)),
@@ -34,18 +34,16 @@ class TestAccessNestedMap(unittest.TestCase):
             (("http://holberton.io"), {"payload": True})
         ])
         
-        def test_get_json(self, test_url, test_payload, mock_get):
-            mock_response = Mock()
+        def test_get_json(self, test_url, test_payload, mock_request):
 
+            mock_response = MagicMock()
             mock_response.json.return_value = test_payload
+            
+            mock_request.get.return_value = mock_response
 
-            mock_get.return_value = mock_response
+            mock_request.assert_called_once_with(test_url)
 
-            result = utils.get_json(test_url)
-
-            mock_get.assert_called_once_with(test_url)
-
-            self.assertEqual(result, test_payload)
+            self.assertEqual(utils.get_json(test_url), test_payload)
 
 
 
@@ -57,24 +55,17 @@ class TestAccessNestedMap(unittest.TestCase):
                 def a_method(self):
                     return 42
 
-                @utils.memoize
+                @memoize
                 def a_property(self):
                     return self.a_method()
-                
 
             instance = TestClass()
 
             with patch.object(instance, 'a_method', return_value=42) as mock_a_method:
-               
                 result1 = instance.a_property()
-
-            
                 result2 = instance.a_property()
-
-                
                 self.assertEqual(result1, 42)
                 self.assertEqual(result2, 42)
-
                 mock_a_method.assert_called_once()
 
 
